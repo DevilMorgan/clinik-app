ae3c2c10756a6c68966bbf38283be52e10190210
Nicolas-Moreno-arevalo <75846359+Nicolas-Moreno-arevalo@users.noreply.github.com>
2021-10-22 17:56:16 -0500

    avance comando para crear cliente

---
+++ app/Console/Commands/Tenant/CreateTenant.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Console\Commands\Tenant;
+
+use App\Models\Tenant\Autorization\Module;
+use App\Models\Tenant\Autorization\Role;
+use App\Models\Tenant\CardType;
+use App\Models\Tenant\User;
+use Hyn\Tenancy\Database\Connection;
+use Hyn\Tenancy\Environment;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+use Hyn\Tenancy\Models\Hostname;
+use Hyn\Tenancy\Models\Website;
+use Hyn\Tenancy\Repositories\HostnameRepository;
+use Hyn\Tenancy\Repositories\WebsiteRepository;
+
+class CreateTenant extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tenant:create {fqdn} {email} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Given a unique tenant name, creates a new tenant in the system.';
+
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // This will be the complete website name (tenantUser.mysite.com)
+        $fqdn = sprintf('%s.%s', $this->argument('fqdn'), env('APP_DOMAIN'));
+
+        // The website object will save the tenant instance information
+        // I recommend to use something random for security reasons
+        $website = new Website;
+        $website->uuid = Str::random(10);
+        app(WebsiteRepository::class)->create($website);
+
+        // The hostname object will save the tenant hosting information, and will be related to the previous created software.
+        $hostname = new Hostname;
+        $hostname->fqdn = $fqdn;
+        $hostname = app(HostnameRepository::class)->create($hostname);
+
+        app(HostnameRepository::class)->attach($hostname, $website);
+
+
+        //(new RouteProvider(app()))->boot();
+        app(Environment::class)->hostname($hostname);
+
+        //add new roles
+        $roles = [
+            ['name' => 'Manager'],
+            ['name' => 'Operative'],
+            ['name' => 'Administrative']
+        ];
+        Role::create($roles);
+
+        //add new modules
+        $modules = [
+            ['name'  => 'Users', 'slug'  => 'users', 'status'=> 1, 'role_id' => 1],
+            ['name'  => 'Manager Medical History', 'slug'  => 'users', 'status'=> 1, 'role_id' => 1],
+            ['name'  => 'Patients Operative', 'slug'  => 'patients-operative', 'status'=> 1, 'role_id' => 2],
+            ['name'  => 'Calendar', 'slug'  => 'calendar', 'status'=> 1, 'role_id' => 2],
+            ['name'  => 'Patients Administrative', 'slug'  => 'patients-administrative', 'status'=> 1, 'role_id' => 3],
+            ['name'  => 'Billing', 'slug'  => 'billing', 'status'=> 0, 'role_id' => 3],
+        ];
+        Module::create($modules);
+
+        //Create new cards type
+        $cardTypes = [
+            ['name' => 'Tarjetas de identidad', 'name_short' => 'TI'],
+            ['name' => 'Cédula de ciudadanía', 'name_short' => 'CC'],
+            ['name' => 'Cédula extranjera', 'name_short' => 'CE'],
+            ['name' => 'Tarjeta extranjera', 'name_short' => 'TE'],
+            ['name' => 'NIP', 'name_short' => 'NIP'],
+            ['name' => 'Tipo de documento extranjero', 'name_short' => 'TDE'],
+        ];
+        CardType::create($cardTypes);
+
+        //add user manager
+        User::craete([
+            'email' => $this->argument('email'),
+            'password' => Hash::make($this->argument('password')),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
+
+        return Command::SUCCESS;
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+//Artisan::command('tenant:create {fqdn} {email} {password}', function ($fqdn , $email, $password) {
+//    $this->info("Sending email to: {$fqdn}!");
+//});

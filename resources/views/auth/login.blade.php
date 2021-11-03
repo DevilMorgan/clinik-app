53cdc32ea9837a0a4670991e3d05d1d9d601e6aa
Nicolas-Moreno-arevalo <75846359+Nicolas-Moreno-arevalo@users.noreply.github.com>
2021-11-03 12:09:55 -0500

    implementar login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,78 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+    <link rel="shortcat icon" href="{{asset('/img/logos/zaabrasalud-favicon.png')}}">
+    <link rel="icon" href="{{asset('/img/logos/zaabrasalud-favicon.png')}}">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600;700&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    <!--    Css plugins    -->
+    <link rel="stylesheet" href="{{ asset('plugin/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugin/sweetalert2/sweetalert2.min.css') }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    <!--    Css    -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+    <title> Login </title>
+</head>
+<body>
+<div class="container">
+    <div class="row main_row my-5">
+        <div class="col-10 sidebar_logo">
+            <img src="{{ asset('img/logo/logo.png') }}" alt="logo">
+        </div>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+        <div class="content_form col-10 col-md-7 col-lg-5">
+            <form method="POST" action="{{ route('login') }}" class="card_login">
+                @csrf
+{{--                <div class="form-group">--}}
+{{--                    <label for="exampleInputEmail1">Shar point</label>--}}
+{{--                    <input type="email" class="form-control" id="exampleInputEmail1">--}}
+{{--                </div>--}}
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <div class="form-group">
+                    <label for="email">{{ __('Email') }}</label>
+                    <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}">
+                </div>
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
+                <div class="form-group">
+                    <label for="password">{{ __('Password') }}</label>
+                    <input type="password" class="form-control" id="password" name="password" autocomplete="current-password">
+                </div>
+                @if(Route::has('password.request'))
+                    <div class="form-group">
+                        <a href="{{ route('password.request') }}" style="text-decoration: underline;">{{ __('Forgot your password?') }}</a>
+                    </div>
+            @endif
+            <!-- <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="remember_me" name="remember">
+                    <label class="form-check-label" for="remember_me">{{ __('Remember me') }}</label>
+                </div> -->
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+                <div class="form-group buttom_save">
+                    <button type="submit" class="btn">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+<!--    Plugins    -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="{{ asset('plugin/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('plugin/sweetalert2/sweetalert2.min.js') }}"></script>
 
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+<!-- Alerts -->
+@include('sweetalert::alert')
+
+
+</body>
+</html>
